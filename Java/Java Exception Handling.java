2d3909ede060e98f0e0ac8cb9b32cbb923d90bc7
IstiakMorsalin <anik.cse@gmail.com>
2015-09-18 00:35:46 +0600

    Exception Handling

---
+++ Java/Java Exception Handling.java
@@ -0,0 +1,9 @@
+class myCalculator {
+    public int power(int a, int b) throws Exception {
+        if (a < 0 || b < 0) {
+            throw new Exception("n and p should be non-negative");
+        } else {
+            return (int) Math.pow((double) a, (double) b);
+        }
+    }
+}
