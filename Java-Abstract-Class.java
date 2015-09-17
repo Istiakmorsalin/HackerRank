3ab76d70336936dcc0f8d79c1ce2a6c7ce1e54a0
IstiakMorsalin <anik.cse@gmail.com>
2015-09-17 17:42:33 +0600

    1st commit

---
+++ Java-Abstract-Class.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+
+    String getTitle() {
+        return title;
+    }
+
+    abstract void setTitle(String s);
+
+}
+
+//Write MyBook class here
+
+class MyBook extends Book {
+
+
+    @Override
+    void setTitle(String s) {
+        this.title=s;
+   }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+
+    }
+}
