a00fd5235f881b0ee40b003452a1c5b2a181aa69
IstiakMorsalin <anik.cse@gmail.com>
2015-09-21 22:07:03 +0600

    java Interface

---
+++ Java/Java Interface.java
@@ -0,0 +1,14 @@
+class MyCalculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int sum = 1;  
+        if (n != 1) sum += n; // since we know divisible by 1 and itself (unless 1)
+        for(int i = 2; i <= (int)Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                sum += i;
+                int d = n/i;
+                if(d != i) sum+=d;
+            }
+        }
+        return sum;
+    }
+} 
