5cfa51e1cb8d0bcc1d7cbf30d88de35e6e096da2
istiak_morsalin <istiak@itconquest.com>
2015-12-21 15:32:57 +0600

    java End Of file

---
+++ Java/Java End Of File.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+	
+public static void main(String[] args) {
+	Scanner cin=new Scanner(System.in);
+	int l=1;
+	for(;cin.hasNext();l++){
+		System.out.println(String.valueOf(l)+" "+cin.nextLine());
+	}
+		
+
+}
+	 
+}//end of 
