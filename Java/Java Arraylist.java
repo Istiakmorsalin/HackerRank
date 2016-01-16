e8c32948835f461f7245c08ed4947367d1a7a460
istiak_morsalin <istiak@itconquest.com>
2016-01-17 01:18:01 +0600

    java arraylist

---
+++ Java/Java Arraylist.java
@@ -0,0 +1,22 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+   public static void main(String []argh)
+   {
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+      in.nextLine();
+      for(int i=0;i<n;i++)
+      {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+         in.nextLine();
+      }
+      while(in.hasNext())
+      {
+         String s=in.nextLine();
+      }
+   }
+}
